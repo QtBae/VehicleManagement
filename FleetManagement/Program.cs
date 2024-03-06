2788aa2e62b4f8c878ec65cff84d30846af4c9f8
Victor Pérez Ngounou <vngounou26@gmail.com>
2024-03-06 16:44:37 +0100

    The most important changes in the code are related to the switch from Tailwind CSS to Bootstrap for the UI framework, the addition of localization features, and the enhancement of the form used to add a new vehicle.
    1. The UI framework has been switched from Tailwind CSS to Bootstrap. This is evident from the changes in `FleetManagement.csproj` where the `Blazorise.Bootstrap` package has been added and `Blazorise.Tailwind` has been removed. The application is also configured to use Bootstrap providers instead of Tailwind in `Program.cs`. The reference to the Tailwind CSS file has been replaced with a reference to the Bootstrap CSS file in `index.html`.
    
    2. Localization features have been added to the project. This is indicated by the addition of the `Microsoft.Extensions.Localization` package in `FleetManagement.csproj` and the configuration of localization services in `Program.cs`. A dropdown for language selection has been added in `MainLayout.razor`, and localized strings for the French and English languages have been added in `VehicleList.razor.fr.resx` and `VehicleList.razor.resx` respectively. A script to manage the user's preferred culture in local storage has also been added in `index.html`.
    
    3. The form used to add a new vehicle in `VehicleList.razor` has been enhanced. It now includes fields for the vehicle's model, brand, year, and mileage. The form title is also displayed using localization.
    
    4. Minor changes have been made in `VehicleListComponent.razor` and `VehicleListComponent.razor.cs`. These include formatting changes, a change in the way the vehicle's model is displayed, and the setting of the `Vehicles` property in the `OnInitialized` method.

--- FleetManagement/Program.cs
+++ FleetManagement/Program.cs
@@ -1,10 +1,12 @@
 using Blazorise;
+using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
-using Blazorise.Tailwind;
 using FleetManagement;
 using FleetManagement.ClientServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,9 +18,32 @@ builder.Services.AddBlazorise(options =>
 {
     options.Immediate = true;
 })
-    .AddTailwindProviders()
+    .AddBootstrapProviders()
     .AddFontAwesomeIcons();
 
 builder.Services.AddScoped<IVehicleServices, VehicleServices>();
 
-await builder.Build().RunAsync();
+//await builder.Build().RunAsync();
+
+builder.Services.AddLocalization();
+
+var host = builder.Build();
+
+CultureInfo culture;
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+if (result != null)
+{
+    culture = new CultureInfo(result);
+}
+else
+{
+    culture = new CultureInfo("en-US");
+    await js.InvokeVoidAsync("blazorCulture.set","en-US");
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
