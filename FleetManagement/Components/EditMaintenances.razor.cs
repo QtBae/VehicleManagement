3e9b29055bd27ad8898a6a923d7657e56595e718
Victor Pérez Ngounou <vngounou26@gmail.com>
2024-03-18 15:31:27 +0100

    add edit Maintenance form and perfom model

---
+++ FleetManagement/Components/EditMaintenances.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared.ApiModels;
+
+namespace FleetManagement.Components
+{
+    public partial class EditMaintenances
+    {
+        [CascadingParameter]
+        public VehicleModel Vehicle { get; set; } = new VehicleModel();
+
+        private MaintainanceModel _newMaintenance = new MaintainanceModel();
+
+        private void AddMaintenance()
+        {
+            if (Vehicle.Maintainances == null)
+            {
+                Vehicle.Maintainances = [];
+            }
+            Vehicle.Maintainances.Add(_newMaintenance);
+        }
+    }
+}
