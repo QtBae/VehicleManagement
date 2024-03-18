8d203d14b09c859aebec85cb3abaae6156302703
jael-dev <jaelngaleu@gmail.com>
2024-03-18 15:42:37 +0100

    Papa Vic wants me to push

---
+++ VehicleManagement/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace VehicleManagement.Data
+{
+    public class DbContextDbContext
+    {
+        public DbSet<Entities.BrandEntity> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+
+        }
+    }
+}
