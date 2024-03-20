28c22ecf96fb2d4e774b168472d83fafbbf59c89
Victor Pérez Ngounou <vngounou26@gmail.com>
2024-03-20 08:59:25 +0100

    perform vehicle data

--- FleetManagement/Data/VehicleData.cs
+++ FleetManagement/Data/VehicleData.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Bogus.DataSets;
 using Shared;
 using Shared.ApiModels;
 
@@ -40,14 +41,16 @@ namespace FleetManagement.Data
         {
             _carModels=GetCarsModels();
             var brands = GetBrands();
+            var _maintenances= GetMaintainanceModels(10);
 
-            var faker = new Faker<VehicleModel>()
+        var faker = new Faker<VehicleModel>()
                 .RuleFor(v => v.Id, f => f.Random.Guid())
                 .RuleFor(v => v.Model, f => f.PickRandom(_carModels))
                 .RuleFor(v => v.Brand, f => f.PickRandom(brands))
                 .RuleFor(v => v.LicensePlate, f => f.Vehicle.Vin())
                 .RuleFor(v => v.Year, f => f.Random.Int(2000, 2020))
                 .RuleFor(v => v.Mileage, f => f.Random.Int(0, 100000))
+                .RuleFor(v => v.Maintainances, _ => (_maintenances))
                 .RuleFor(b => b.Energy,f => f.PickRandom<Energy>());
 
             return faker.Generate(100);
@@ -59,8 +62,7 @@ namespace FleetManagement.Data
                var faker = new Faker<CarModel>()
                 .RuleFor(c => c.Id, f => f.Random.Guid())
                 .RuleFor(b => b.MaintenanceFrequency,f => f.Random.Int(1,100))
-                .RuleFor(c => c.ModelName, f => f.Vehicle.Model())
-                .RuleFor(c => c.MaintenanceFrequency, f => f.Random.Int(1, 100));
+                .RuleFor(c => c.ModelName, f => f.Vehicle.Model());
 
             return faker.Generate(nbr);
         }
@@ -73,6 +75,17 @@ namespace FleetManagement.Data
                 .RuleFor(b => b.BrandName,f => f.Vehicle.Manufacturer());
 
             return faker.Generate(nbr);
-        }   
+        }
+        
+        public static IEnumerable<MaintainanceModel> GetMaintainanceModels(int nbr=100)
+        {
+            var faker = new Faker<MaintainanceModel>()
+                .RuleFor(m => m.Id, f => f.Random.Guid())
+                .RuleFor(m => m.Works, f => f.Vehicle.Manufacturer())
+                .RuleFor(m => m.Date,f => f.Date.Past(2))
+                .RuleFor(m => m.Mileage,f => f.Random.Int(0,100000));
+
+            return faker.Generate(nbr);
+        }
     }
 }
