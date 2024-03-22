46b98cc3ef01bb472ebe6bbc4b027d8f4cc18f87
Victor Pérez Ngounou <vngounou26@gmail.com>
2024-03-22 14:27:39 +0100

    ensure swaguer is generated

--- VehicleManagement/Program.cs
+++ VehicleManagement/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
